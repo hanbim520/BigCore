70c751dea8487dc056bb70fdd32e98cf8ca22687
张海军 <710605420@qq.com>
2025-04-24 16:53:18 +0800

    add

--- Android/CpuInfoTest/app/src/main/cpp/GetAndroidInfo.h
+++ Android/CpuInfoTest/app/src/main/cpp/GetAndroidInfo.h
@@ -5,11 +5,18 @@
 #ifndef CPUINFOTEST_GETANDROIDINFO_H
 #define CPUINFOTEST_GETANDROIDINFO_H
 #include <jni.h>
+#include <sched.h>  // for sched_getcpu
+#include <stdio.h>
 
 class GetAndroidInfo {
 public:
     static int GetBatteryLevel(JNIEnv* Env, jobject Context);
     static float GetBatteryTemperature(JNIEnv* Env, jobject Context);
+
+    static int GetCurrentCPU()
+    {
+        return sched_getcpu();
+    }
 };
 
 
--- Android/CpuInfoTest/app/src/main/cpp/native-lib.cpp
+++ Android/CpuInfoTest/app/src/main/cpp/native-lib.cpp
@@ -19,14 +19,17 @@ Java_com_example_cpuinfotest_MainActivity_stringFromJNI(
     std::vector<CpuInfoData>  CPUInfos = androidCpuInfo.GetAllCpus();
     for (const auto& cpu : CPUInfos)
     {
-        auto threads_speed = cpu.GetMaxClockSpeed_MHz();
+        auto max_speed = cpu.GetMaxClockSpeed_MHz();
+        auto min_speed = cpu.GetMinClockSpeed_MHz();
+        auto reg_speed = cpu.GetRegularClockSpeed_MHz();
+        auto cur_speed = cpu.GetCurrentClockSpeed();
        // assert((threads_utility.size() == threads_speed.size()));
 
-        std::cout << "cpu id: " << cpu._processor << " Max ClockSpeed: " <<  threads_speed  << "MHz"<<std::endl;
+        std::cout << "cpu id: " << cpu._processor << " Max ClockSpeed: " <<  max_speed  << "MHz"<< " Min ClockSpeed: " <<  min_speed  << "MHz"<< " Regular ClockSpeed: " <<  reg_speed  << "MHz"<< " Current ClockSpeed: " <<  cur_speed  << "MHz"<< std::endl;
     }
 
 
     std::cout << "BatteryLevel: " << GetAndroidInfo::GetBatteryLevel(env,MainActivity) << " BatteryTemperature : " <<  GetAndroidInfo::GetBatteryTemperature(env,MainActivity) << "°C"<<std::endl;
-
+    std::cout << "Work on cpu Core: " << GetAndroidInfo::GetCurrentCPU()<<std::endl;
     return env->NewStringUTF(hello.c_str());
 }
